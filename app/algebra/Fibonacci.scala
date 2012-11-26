39848183854050e87db47c4b401976aafe97012c
Bartosz Jankiewicz <bartosz.jankiewicz@gmail.com>
2012-11-26 23:20:58 +0100

    Definitions of some basic metrics

---
+++ app/algebra/Fibonacci.scala
@@ -0,0 +1,9 @@
+package algebra
+
+/**
+ * Calculates set of support lines based of Fibonacci's numbers
+ */
+object Fibonacci {
+  
+  def calculate(bottom: Int, top: Int): Seq[Int] = ???
+}
---
+++ app/algebra/Pivot.scala
@@ -0,0 +1,25 @@
+package algebra
+
+import domain.Candle
+import domain.Tick
+
+/**
+ * Support line based on highest/lowest observation
+ */
+class Pivot extends Metric {
+  
+  def update(tick: Tick, candles: Stream[Candle]) = ???
+  
+  def update(candles: Stream[Candle]) = ???
+
+}
+
+object PivotProducer {
+  
+  /**
+   * Abalyses given candles stream and creates new pivot if there is reason for that.
+   */
+  def analyse( candles: Stream[Candle]): Option[Candle] = {
+    ???
+  }
+}
